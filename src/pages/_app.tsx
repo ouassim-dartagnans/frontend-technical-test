9562d6ba1f6ba7c64628ecd3f852139fd2066a0d
wass <ouassim@dartagnans.fr>
2022-09-29 00:50:54 +0200

    ➕ Add React-Query and Prettier to the project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,25 @@
-import type { AppProps } from 'next/app'
-import { getLoggedUserId } from '../utils/getLoggedUserId'
-import '../styles/globals.css'
+import type { AppProps } from 'next/app';
+import { getLoggedUserId } from '../utils/getLoggedUserId';
+import '../styles/globals.css';
+import { useState } from 'react';
+import { Hydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import Layout from '../components/Layout';
 
 // Default way to get a logged user
-export const loggedUserId = getLoggedUserId()
+export const loggedUserId = getLoggedUserId();
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Hydrate>
+    </QueryClientProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
